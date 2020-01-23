0a7024466c9a2cad09dfea93304e3c9a9ac50aec
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-22 23:50:39 -0700

    styled html pages

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res) => {
--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "views", "store.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "store.html"));
 });
 
 module.exports = router;
