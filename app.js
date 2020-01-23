d361a74ad3489c1ae6dd66cdae893488ffd728bd
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-22 17:21:40 -0700

    added 404 not found html page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -8,7 +9,11 @@ const shopRoutes = require("./routes/store");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
+});
 
 app.listen(3000);
