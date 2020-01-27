fe4809ad8c5abadde6693004c167e3ee49c1180c
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-26 23:22:47 -0700

    added root directory function to help routing to html pages

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const rootDirectory = require("../util/path");
+
 router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.sendFile(path.join(rootDirectory, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res) => {
--- routes/store.js
+++ routes/store.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const rootDirectory = require("../util/path");
+
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "views", "store.html"));
+  res.sendFile(path.join(rootDirectory, "views", "store.html"));
 });
 
 module.exports = router;
