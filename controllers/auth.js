58333ca0a27d800bae24c54967b2b3f949fd4395
bigdub3298 <wesleyaustin33@gmail.com>
2020-03-25 22:36:48 -0600

    added validation for all forms

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const Sequelize = require("sequelize");
 const bcrypt = require("bcrypt");
 const mailer = require("../sendgrid");
 const crypto = require("crypto");
+const { validationResult } = require("express-validator/check");
 
 const User = require("../models/user");
 const Cart = require("../models/cart");
@@ -22,48 +23,39 @@ exports.getLoginPage = (req, res) => {
 exports.postLoginPage = (req, res) => {
   const { email, password } = req.body;
 
-  User.findOne({ where: { email } })
-    .then(user => {
-      if (!user) {
-        req.flash("error", "The email or password you entered is incorrect.");
-        return req.session.save(err => {
-          if (err) {
-            console.log(err);
-          }
-          res.redirect("/login");
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(422).render("auth/login", {
+      docTitle: "Login",
+      path: "/login",
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
+  bcrypt
+    .compare(password, req.user.password)
+    .then(doMatch => {
+      if (!doMatch) {
+        return res.render("auth/login", {
+          docTitle: "Login",
+          path: "/login",
+          errorMessage: "The email or password you entered is incorrect."
         });
       }
 
-      bcrypt
-        .compare(password, user.password)
-        .then(doMatch => {
-          if (!doMatch) {
-            req.flash(
-              "error",
-              "The email or password you entered is incorrect."
-            );
-            return req.session.save(err => {
-              if (err) {
-                console.log(err);
-              }
-              res.redirect("/login");
-            });
-          }
-
-          req.session.userId = user.id;
-          req.session.save(err => {
-            if (err) {
-              console.log(err);
-            }
-            res.redirect("/");
-          });
-        })
-        .catch(err => {
+      req.session.userId = req.user.id;
+      req.session.save(err => {
+        if (err) {
           console.log(err);
-          res.redirect("/login");
-        });
+        }
+        res.redirect("/");
+      });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      res.redirect("/login");
+    });
 };
 
 exports.postLogoutPage = (req, res) => {
@@ -86,40 +78,36 @@ exports.getSignUpPage = (req, res) => {
 };
 
 exports.postSignUpPage = (req, res) => {
-  const { email, password, confirmedPassword } = req.body;
+  const { email, password } = req.body;
 
-  User.findOne({ where: { email } })
-    .then(user => {
-      if (!user) {
-        return bcrypt
-          .hash(password, 12)
-          .then(hashedPassword =>
-            User.create(
-              { email, password: hashedPassword, cart: {} },
-              { include: [Cart] }
-            )
-          )
-          .then(_ => {
-            const message = {
-              to: email,
-              from: process.env.EMAIL,
-              subject: "Thanks For Signing Up With SimpleStore",
-              html: "<h1>Your sign up was successful!</h1>"
-            };
+  const errors = validationResult(req);
 
-            res.redirect("/login");
-            return mailer.send(message);
-          })
-          .catch(err => console.log(err));
-      }
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      docTitle: "Sign Up",
+      path: "/signup",
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
-      req.flash("error", "An account with this email already exists.");
-      return req.session.save(err => {
-        if (err) {
-          console.log(err);
-        }
-        res.redirect("/signup");
-      });
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword =>
+      User.create(
+        { email, password: hashedPassword, cart: {} },
+        { include: [Cart] }
+      )
+    )
+    .then(_ => {
+      const message = {
+        to: email,
+        from: process.env.EMAIL,
+        subject: "Thanks For Signing Up With SimpleStore",
+        html: "<h1>Your sign up was successful!</h1>"
+      };
+
+      res.redirect("/login");
+      return mailer.send(message);
     })
     .catch(err => console.log(err));
 };
@@ -136,6 +124,19 @@ exports.getPasswordResetPage = (req, res) => {
 
 exports.postPasswordResetPage = (req, res) => {
   const { email } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/reset", {
+      docTitle: "Password Reset",
+      path: "/reset",
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
+  const user = req.user;
+
   crypto.randomBytes(32, (err, buffer) => {
     if (err) {
       console.log(err);
@@ -143,35 +144,22 @@ exports.postPasswordResetPage = (req, res) => {
     }
 
     const token = buffer.toString("hex");
-    User.findOne({ where: { email } })
-      .then(user => {
-        if (!user) {
-          req.flash("error", "No account with that email exists.");
-          return req.session.save(err => {
-            if (err) {
-              console.log(err);
-            }
-            res.redirect("/reset");
-          });
-        }
 
-        user.resetToken = token;
-        user.resetTokenExpiration = Date.now() + 1600000;
-        user.save().then(user => {
-          const message = {
-            to: email,
-            from: process.env.EMAIL,
-            subject: "Password Reset",
-            html: `
+    user.resetToken = token;
+    user.resetTokenExpiration = Date.now() + 1600000;
+    user.save().then(_ => {
+      const message = {
+        to: email,
+        from: process.env.EMAIL,
+        subject: "Password Reset",
+        html: `
             <p>You requested a password reset</p>
             <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>
             `
-          };
-          res.redirect("/");
-          return mailer.send(message);
-        });
-      })
-      .catch(err => console.log(err));
+      };
+      res.redirect("/");
+      return mailer.send(message);
+    });
   });
 };
 
@@ -209,7 +197,19 @@ exports.getNewPasswordPage = (req, res) => {
 };
 
 exports.postNewPasswordPage = (req, res) => {
-  const { password, confirmedPassword, userId, token } = req.body;
+  const { password, userId, token } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.render("auth/new-password", {
+      docTitle: "New Password",
+      path: "/reset",
+      errorMessage: errors.array()[0].msg,
+      userId,
+      token
+    });
+  }
 
   User.findOne({
     where: {
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,100 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator/check");
+
+const User = require("../models/user");
 
 const authController = require("../controllers/auth");
 
 router.get("/login", authController.getLoginPage);
-router.post("/login", authController.postLoginPage);
+router.post(
+  "/login",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        return User.findOne({ where: { email: value } }).then(user => {
+          if (!user) {
+            return Promise.reject(
+              "The email or password you entered is incorrect."
+            );
+          }
+          req.user = user;
+        });
+      })
+  ],
+  authController.postLoginPage
+);
+
 router.post("/logout", authController.postLogoutPage);
+
 router.get("/signup", authController.getSignUpPage);
-router.post("/signup", authController.postSignUpPage);
+
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        return User.findOne({ where: { email: value } }).then(user => {
+          if (user) {
+            return Promise.reject("An account with this email already exists.");
+          }
+        });
+      }),
+    check(
+      "password",
+      "Please enter a password with at least 8 characters."
+    ).isLength({ min: 8 }),
+    check("confirmedPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords must match.");
+      }
+      return true;
+    })
+  ],
+  authController.postSignUpPage
+);
+
 router.get("/reset", authController.getPasswordResetPage);
-router.post("/reset", authController.postPasswordResetPage);
+
+router.post(
+  "/reset",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        return User.findOne({ where: { email: value } }).then(user => {
+          if (!user) {
+            return Promise.reject("No account with that email exists.");
+          }
+          req.user = user;
+        });
+      })
+  ],
+  authController.postPasswordResetPage
+);
+
 router.get("/reset/:token", authController.getNewPasswordPage);
-router.post("/new-password", authController.postNewPasswordPage);
+
+router.post(
+  "/new-password",
+  [
+    check(
+      "password",
+      "Please enter a password with at least 8 characters."
+    ).isLength({ min: 8 }),
+    check("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords must match.");
+      }
+      return true;
+    })
+  ],
+  authController.postNewPasswordPage
+);
 
 module.exports = router;
